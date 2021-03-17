d25a00e9347b05f52da98e1fe10fa6fe19b7ccf5
Kazuorz <albo-25@hotmail.com>
2021-03-16 21:23:26 -0300

    adding some wip trash that doesnt work i give up

--- src/components/BeatmapCards/BeatmapCardSimple.jsx
+++ src/components/BeatmapCards/BeatmapCardSimple.jsx
@@ -1,60 +1,71 @@
 /* eslint-disable eqeqeq */
 import React from "react";
 import { Link } from "react-router-dom";
-
+import useApi from "../../hooks/useApi";
 const BeatmapCardSimple = (props) => {
   function handleColors(modifier) {
     if (modifier == "no_mod") {
-      return "bg-gray-400";
+      return "from-gray-400 to-gray-600";
     }
     if (modifier == "hard_rock") {
-      return "bg-red-500";
+      return "from-red-400 to-red-900";
     }
     if (modifier == "double_time") {
-      return "bg-purple-600";
+      return "from-purple-500 to-purple-800";
     }
     if (modifier == "hidden") {
-      return "bg-blue-300";
+      return "from-blue-300 to-blue-500";
     }
     if (modifier == "free_mods") {
-      return "bg-green-600";
+      return "from-green-500 to-green-800";
     }
     if (modifier == "flashlight") {
       return "bg-blue-900";
     }
     if (modifier == "tie_breaker") {
-      return "bg-gray-900";
+      return "from-gray-600 to-black";
     }
   }
 
   return (
-    <Link
-      to={{
-        pathname: `/beatmaps/${props.reference._id}`,
-      }}
-    >
-      <div className="rounded-xl overflow-auto relative text-center">
-        <section
-          className="p-4 h-28"
-          style={{
-            backgroundSize: "cover",
-            backgroundBlendMode: "difference",
-            backgroundImage: `linear-gradient(rgba(255,255,255, .4), rgba(255,255,255, .4)), url(${props.reference.covers.cover})`,
-          }}
-        >
-          <h2 className="text-white text-lg truncate ...">{props.reference.title}</h2>
-          <h3 className="text-white text-base truncate ...">{props.reference.artist}</h3>
-          <p className="font-bold text-lg truncate ...">{props.difficulty}</p>
-        </section>
-        <div
-          className={
-            "p-4 py-2 text-white text-center " + handleColors(props.modifiers)
-          }
-        >
-          <p className="capitalize">{props.modifiers}</p>
+    <div>
+      <Link
+        to={{
+          pathname: `/beatmaps/${props.reference._id}`,
+        }}
+      >
+        <div className="rounded-t-xl overflow-auto relative text-center">
+          <section
+            className="p-4 h-28"
+            style={{
+              backgroundSize: "cover",
+              backgroundBlendMode: "difference",
+              backgroundImage: `linear-gradient(rgba(0,0,0, .4), rgba(255,255,255, .4)), url(${props.reference.covers.cover})`,
+            }}
+          >
+            <div className="">
+              <h2 className="text-white text-lg truncate ...">
+                {props.reference.title}
+              </h2>
+              <h3 className="text-white text-base truncate ...">
+                {props.reference.artist}
+              </h3>
+              <p className="font-bold text-white text-xl truncate ...">
+                {props.difficulty}
+              </p>
+            </div>
+          </section>
         </div>
+      </Link>
+      <div
+        className={
+          "flex rounded-b-xl justify-center p-4 py-2 text-white bg-gradient-to-r " +
+          handleColors(props.modifiers)
+        }
+      >
+        <p className="capitalize font-bolder text-xl">{props.modifiers}</p>
       </div>
-    </Link>
+    </div>
   );
 };
 
--- src/components/Pool/Pool.jsx
+++ src/components/Pool/Pool.jsx
@@ -1,16 +1,67 @@
-import React from 'react'
-import BeatmapCardSimple from "../BeatmapCards/BeatmapCardSimple"
+import React from "react";
+import BeatmapCardSimple from "../BeatmapCards/BeatmapCardSimple";
+import { FaTrash } from "react-icons/fa";
+import useApi from "../../hooks/useApi";
+import { methods } from "../../services/axios";
 const Pool = (props) => {
-    return (
-        <div>
-            {console.log(props)}
-            <h1 className="text-center text-4xl py-8">{props.name}</h1>
-            <div className="grid grid-cols-3 gap-4">
-                {props.beatmapsets.map((beatmap, index)=>
-                <BeatmapCardSimple {...props.beatmapsets[index]}/>)}
-            </div>
-        </div>
-    )
-}
+  async function updatePool(payload) {
+    // await methods.patch({
+    //     url: `pools/${props._id}`,
+    //     data: {
+    //         beatmapsets: [
+    //             payload[0],
+    //         ]
+    //     }
+    // })
+    console.log(payload[0])
+  }
 
-export default Pool
+  const handleDelete = (id) => {
+    const payload = props;
+    const found = payload.beatmapsets.findIndex(
+      (beatmapset) => beatmapset._id === id
+    );
+    if (found !== -1) {
+      const deleteFound = payload.beatmapsets.splice(found, 1);
+      
+      const mapped = payload.beatmapsets.map((beatmapset) => ({
+        reference: beatmapset._id,
+        modifiers: beatmapset.modifiers,
+        difficulty: beatmapset.difficulty,
+      }));
+      updatePool(mapped);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="text-center text-4xl py-8">{props.name}</h1>
+
+      <p className="text-2xl font-bold">
+        {" "}
+        This pool's status is "{props.status}".
+      </p>
+      <p className="text-lg pb-8">
+        This means{" "}
+        {props.status === "draft"
+          ? "you can edit it."
+          : "you cannot modify it anymore."}
+      </p>
+      <div className="grid grid-cols-3 gap-4">
+        {props.beatmapsets.map((beatmap, index) => (
+          <div>
+            <BeatmapCardSimple {...props.beatmapsets[index]} />
+            <button
+              className="text-white opacity-50 hover:opacity-100 float-right"
+              onClick={() => handleDelete(props.beatmapsets[index]._id)}
+            >
+              <FaTrash />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Pool;
