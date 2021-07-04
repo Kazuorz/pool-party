313eb84c090b7d71a9d6fa204e63246f4935eda7
Agustín González <8430727+agonzalezmurua@users.noreply.github.com>
2021-07-03 20:40:21 -0400

    feat: added proxy for local server for development

--- src/routes/tournaments.jsx
+++ src/routes/tournaments.jsx
@@ -3,7 +3,9 @@ import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import { FaSearch } from "react-icons/fa";
+
 import TournamentCardsContainer from "../components/TournamentCards/TournamentCardsContainer"
+
 const Tournaments = () => {
   return (
     <div>
